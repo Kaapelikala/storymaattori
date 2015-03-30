a9201565edbc844a10d523628ea8f35a40d579e6
Kalle <test@example.com>
2015-03-30 17:35:32 +0300

    Muutama scripti

---
+++ Assets/scripts/Soldier.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Storymaattori
+{
+	public class Soldier : Critter
+	{
+		private int id;
+		private string callsign;
+		private int experience;
+		private int traits;
+
+		public Soldier (int recruitmentSelection)
+		{
+		}
+
+		public Soldier ()
+		{
+		}
+
+		public string GetCallsign ()
+		{
+			return callsign;
+		}
+		public void SetCallsign()
+		{
+			//TextGenerator generates new
+		}
+
+		public void SetCallsign(string callsign)
+		{
+			this.callsign = callsign;
+		}
+
+		public int GetLevel ()
+		{
+			return (int)(experience % 1000);
+		}
+
+		public int AddExperience (int experience)
+		{
+			this.experience = this.experience + experience;
+			return this.GetLevel ();
+		}
+
+		public bool HasTrait (int trait)
+		{
+			if (traits & trait == trait)
+				return true;
+			else
+				return false;
+		}
+
+	}
+}
