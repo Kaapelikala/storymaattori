2eac50ccbbdcde16c1ff82f20c0a1343a6b2bfd0
Kalle <test@example.com>
2015-05-15 05:14:34 +0300

    ButtonController that switches views on and off

---
+++ Assets/scripts/ButtonController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+	public GameObject soldierView;
+	public GameObject missionView;
+	public GameObject deadSoldierView;
+	public GameObject mainView;
+
+
+	public void ActivateSoldierViewButton(){
+		soldierView.SetActive (true);
+		mainView.SetActive (false);
+	}
+
+	public void ActivateDeadSoldierViewButton(){
+		deadSoldierView.SetActive (true);
+		mainView.SetActive (false);
+	}
+
+	public void ActivateMissionViewButton(){
+		missionView.SetActive (true);
+		mainView.SetActive (false);
+	}
+
+	public void DeActivateSoldierViewButton(){
+		soldierView.SetActive (false);
+		mainView.SetActive (true);
+	}
+	
+	public void DeActivateDeadSoldierViewButton(){
+		deadSoldierView.SetActive (false);
+		mainView.SetActive (true);
+	}
+	
+	public void DeActivateMissionViewButton(){
+		missionView.SetActive (false);
+		mainView.SetActive (true);
+	}
+
+
+}
