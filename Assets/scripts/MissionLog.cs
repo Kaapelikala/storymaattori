940ed94fd9f57b21576c4aeb2ea3a82093dcaecf
Kalle <test@example.com>
2015-05-20 14:57:25 +0300

    Missionlog

--- Assets/scripts/MissionLog.cs
+++ Assets/scripts/MissionLog.cs
@@ -4,24 +4,19 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class MissionLog : MonoBehaviour {
-
+	
 	public Text missionText;
 	public List<Mission> missions;
 	public int currentlyAdded;
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
 		currentlyAdded = -1;
 		missions = new List<Mission>();
 		missionText.text="";
-=======
-		//missions = new List<Mission>;
-		//missionText.text="";
->>>>>>> d9dcb7a3ba915ef9a7d61171742594ef01f19c00
 	}
-
+	
 	public void UpdataLog()
 	{
 		currentlyAdded++;
