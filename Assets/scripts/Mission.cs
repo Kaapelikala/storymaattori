068c2247403795b3c7d8ed38cce282f10183e690
Kalle <test@example.com>
2015-05-20 14:53:56 +0300

    lol

--- Assets/scripts/Mission.cs
+++ Assets/scripts/Mission.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Mission : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public string location;
+	public List<SoldierController> squad;
+	public string type;
+	public int difficulty;
+
+
+	override public string ToString()
+	{
+		string returned = "";
+		returned += "Location: " + location + "\n";
+		returned += "Operation: " + type + "\n";
+		returned += "Members:\n";
+		foreach (SoldierController soldier in squad) {
+			returned +=soldier.soldierFName+" '"+soldier.callsign+"' "+soldier.soldierLName+"\n";
+		}
+		returned += "During the mission: ";
+		foreach (SoldierController soldier in squad) {
+			returned +=soldier.soldierFName+" '"+soldier.callsign+"' "+soldier.soldierLName+" managed to:\n";
+			returned+=soldier.GetEvents
+		}
+		return returned;
 	}
 }
--- Assets/scripts/MissionLog.cs
+++ Assets/scripts/MissionLog.cs
@@ -7,16 +7,19 @@ public class MissionLog : MonoBehaviour {
 
 	public Text missionText;
 	public List<Mission> missions;
+	public int currentlyAdded;
 
 
 	// Use this for initialization
 	void Start () {
-		missions = new List<Mission>;
+		currentlyAdded = -1;
+		missions = new List<Mission>();
 		missionText.text="";
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void UpdataLog()
+	{
+		currentlyAdded++;
+		missionText.text = missionText.text + missions [0].ToString;
 	}
 }
