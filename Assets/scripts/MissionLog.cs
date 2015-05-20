1818892dc3695a39b1849f414077191cfdda05a5
Kalle <test@example.com>
2015-05-20 14:32:03 +0300

    missionlog

--- Assets/scripts/MissionLog.cs
+++ Assets/scripts/MissionLog.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class MissionLog : MonoBehaviour {
 
+	public Text missionText;
+	public List<Mission> missions;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		missions = new List<Mission>;
+		missionText.text="";
 	}
 	
 	// Update is called once per frame
